bf82238c2db03c2d407ebd4684b46db4de1fab95
removedporn <86824510+removedporn@users.noreply.github.com>
2022-07-03 12:14:12 +0800

    Delete server directory

--- server/src/main/java/com/okta/developer/demo/beer/BeerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.okta.developer.demo.beer;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource
-interface BeerRepository extends JpaRepository<Beer, Long> {
-}
